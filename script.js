f685a871c7dfd1fd1533fc0fd654a5131ac27a07
homework69 <83495211+homework69@users.noreply.github.com>
2021-05-01 14:30:26 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-var myName = prompt('введите свое имя')
-var age = +prompt('введите свой возраст')
-console.log( 'Вас зовут ' + myName + ' вам ' + age + ' лет' );
-
-
-var plus = +prompt('6 + 3 = ?')
-console.log('6 + 3 = ' + ( 6 + 3)  + ' (ваш ответ) ' + plus);
-
-var minus = +prompt('6 - 3 = ?')
-console.log('6 - 3 = ' + ( 6 - 3) + ' (ваш ответ) ' + minus);
-
-var umn = +prompt('6 * 3 = ?')
-console.log('6 * 3 = ' + ( 6 * 3) + ' (ваш ответ) ' + umn );
-
-var del = +prompt('6 / 3 = ?')
-console.log('6 / 3 = ' + ( 6 / 3) + ' (ваш ответ) ' + del);
-
-var proc = +prompt('6 % 3 = ?')
-console.log('6 % 3 = ' + ( 6 % 3) + ' (ваш ответ) ' + proc);
+var sec = prompt('введите секунды')
+console.log('В минутах: ' + ( sec / 60) );
+console.log('В часах: ' + (sec / 3600));
+console.log('В сутках ' + (sec / 86400));
