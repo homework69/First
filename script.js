ed005ea6f81eab29885fdcd53fe9bb407f87a232
homework69 <83495211+homework69@users.noreply.github.com>
2021-05-09 13:39:56 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-var sec = prompt('введите секунды')
-console.log('В минутах: ' + ( sec / 60) );
-console.log('В часах: ' + (sec / 3600));
-console.log('В сутках ' + (sec / 86400));
+var num = +prompt('Введите кол-во ступеней')
+var sim1 = prompt('Введите отступы')
+var sim2 = prompt('Введите конечный символ')
+for (let i = 0; i < num; i++) {
+    if (i == 0 ){
+        sim2 = sim2
+    }else{
+        sim2 = sim1 + sim2;
+    }
+    console.log(sim2);
+}
